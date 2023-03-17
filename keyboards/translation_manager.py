4cef991e625cb736309767cd0160f1937c783d47
RTM <taras.rybak.2001@gmail.com>
2023-03-18 01:23:26 +0200

    trans

--- keyboards/translation_manager.py
+++ keyboards/translation_manager.py
@@ -1,6 +1,9 @@
 import os
 from google.cloud import translate_v2 as translate
 import localization
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r'C:/Users/vikaz/OneDrive/Рабочий стол/Work/Project/red_repo/translater-bot-intern-b9dec3a171bd.json'
+# project_id = 'translater-bot-intern'
+parent = f"projects/{project_id}"
 
 def translate_messages(target_language: str):
     translate_client = translate.Client()
